c6dbbee381cc8da0735566e7bfc8b22a9b09a6fa
Tony Cook <tony@pipesearch.com>
2023-09-03 22:44:40 -0500

    Initial POC

--- App.js
+++ App.js
@@ -1,10 +1,54 @@
+import { useState, useEffect } from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import { mossyBackendDevUrl } from './constants';
 
 export default function App() {
+  const [buttonLabel, setButtonLabel] = useState('loading...')
+
+  useEffect(() => {
+    /* setTimeout(() => {
+      setButtonLabel('press me')
+    }, 3000) */
+    async function fetchStuff() {
+      const config = {
+        method: 'GET',
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+      let result
+      try {
+        const response = await fetch(`http://${mossyBackendDevUrl}/api/books`, config)
+        const serializedResponse = await response.json()
+        console.log('serializedResponse', serializedResponse)
+        result = serializedResponse.author
+      } catch (err) {
+        console.log('err', err)
+        result = err.message
+      }
+      setButtonLabel(result)
+    }
+    fetchStuff()
+  }, [])
+
+  function handleButtonPress() {
+    if (buttonLabel === 'press me again') {
+      setButtonLabel('i done been pressed')
+    } else {
+      setButtonLabel('press me again')
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>mossy</Text>
+      <Text>you have work to do</Text>
+      <Button
+        title={buttonLabel}
+        onPress={handleButtonPress}
+      />
       <StatusBar style="auto" />
     </View>
   );
